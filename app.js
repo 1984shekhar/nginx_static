c1b29ed181163932e2bf01db3bcc135c740b63df
1984shekhar <chandrashekharpandey@hotmail.com>
2021-04-01 22:38:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 async function loadPage(){
-    const response = await fetch("http://localhost:8080/hello-resteasy");
+    const response = await fetch("http://quarkuscors-quarkus.apps.openshift4rdu.lab.upshift.rdu2.redhat.com:8080/hello-resteasy");
     let message = await response.text();
     console.log(`${message} from quarkus resteasy application`);
     document.body.innerHTML = message;
